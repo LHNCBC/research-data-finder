3206289fa10d2261ac13f53bdbafe3509cd53878
xieh3 <hao.xie@nih.gov>
2021-03-22 19:33:59 -0400

    IntersectionObserver for scroll watching

--- next/src/app/modules/step-3-view-cohort-page/view-cohort-page.component.ts
+++ next/src/app/modules/step-3-view-cohort-page/view-cohort-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import Bundle = fhir.Bundle;
 import BundleEntry = fhir.BundleEntry;
 import {HttpClient} from "@angular/common/http";
@@ -14,14 +14,43 @@ import {HttpClient} from "@angular/common/http";
 export class ViewCohortPageComponent implements OnInit {
   patientColumns: string[] = ['id', 'url'];
   patientDataSource: BundleEntry[] = [];
+  nextBundleUrl: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.http.get('https://lforms-fhir.nlm.nih.gov/baseR4/Patient?_elements=id,name,birthDate,active&_count=100')
+    // TODO: temporarily calling this test server manually here
+    this.callBatch('https://lforms-fhir.nlm.nih.gov/baseR4/Patient?_elements=id,name,birthDate,active&_count=100');
+  }
+
+  callBatch(url: string) {
+    this.http.get(url)
       .subscribe((data: Bundle) => {
-        this.patientDataSource = data.entry;
+        this.nextBundleUrl = data.link.find(l => l.relation === 'next')?.url;
+        this.patientDataSource = this.patientDataSource.concat(data.entry);
+        if (this.nextBundleUrl) { // if bundle has no more 'next' link, do not create watcher for scrolling
+          this.createIntersectionObserver();
+        }
       });
+  }
+
+  createIntersectionObserver() {
+    this.cd.detectChanges();
+    // last row element of what's rendered
+    let lastResourceElement = document.getElementById(this.patientDataSource[this.patientDataSource.length - 1].resource.id);
+    let observer = new IntersectionObserver((entries, obs) => {
+      entries.forEach(entry => {
+        // when last row of resource is displayed in viewport, unwatch this element and call next batch
+        if (entry.intersectionRatio > 0) {
+          obs.disconnect();
+          this.callBatch(this.nextBundleUrl);
+        }
+      });
+    });
+    observer.observe(lastResourceElement);
   }
 
 }
