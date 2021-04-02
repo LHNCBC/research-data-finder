39106cba96db19ab775d677765c16d87f69936c5
xieh3 <hao.xie@nih.gov>
2021-04-02 16:39:38 -0400

    tests set up

--- next/src/app/modules/resource-table/resource-table.module.ts
+++ next/src/app/modules/resource-table/resource-table.module.ts
@@ -5,12 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatTableModule} from "@angular/material/table";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatInputModule} from "@angular/material/input";
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from "@angular/material/select";
-
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [ ResourceTableComponent ],
--- next/src/app/modules/step-3-view-cohort-page/view-cohort-page.module.ts
+++ next/src/app/modules/step-3-view-cohort-page/view-cohort-page.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ViewCohortPageComponent } from './view-cohort-page.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {ResourceTableModule} from "../resource-table/resource-table.module";
-
-
+import {ResourceTableModule} from '../resource-table/resource-table.module';
 
 @NgModule({
   declarations: [ViewCohortPageComponent],
