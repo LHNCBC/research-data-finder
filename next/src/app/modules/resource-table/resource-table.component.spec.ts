8774784927eafef5dc6961a8fb55f3612ba45720
xieh3 <hao.xie@nih.gov>
2021-04-03 20:26:03 -0400

    fix tests

--- next/src/app/modules/resource-table/resource-table.component.spec.ts
+++ next/src/app/modules/resource-table/resource-table.component.spec.ts
@@ -1,22 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ResourceTableComponent } from './resource-table.component';
+import {of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {ResourceTableModule} from './resource-table.module';
+import {ColumnDescription} from '../../types/column.description';
 
 describe('ResourceTableComponent', () => {
   let component: ResourceTableComponent;
   let fixture: ComponentFixture<ResourceTableComponent>;
 
+  const bundle = {
+    type: undefined,
+    link: [
+      { relation: 'next', url: '' }
+    ],
+    total: 100,
+    entry: []
+  };
+  const columnDescriptions: ColumnDescription[] = [
+    {
+      displayName: 'ID',
+      element: 'id',
+      types: ['string'],
+      isArray: false,
+      visible: false
+    }
+  ];
+
   beforeEach(async () => {
+    const spies = [];
+    spies.push(jasmine.createSpyObj('HttpClient', ['get']));
+    spies[0].get.and.returnValue(of(bundle));
+
     await TestBed.configureTestingModule({
-      declarations: [ ResourceTableComponent ]
+      declarations: [ ResourceTableComponent ],
+      imports: [
+        ResourceTableModule
+      ],
+      providers: [
+        { provide: HttpClient, useValue: spies[0] }
+      ]
     })
     .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(ResourceTableComponent);
     component = fixture.componentInstance;
+    component.columns = [];
+    component.columnDescriptions = columnDescriptions;
+    component.initialBundle = bundle;
     fixture.detectChanges();
+    await fixture.whenStable();
   });
 
   it('should create', () => {
