1b2a8caaee99defb3f31ee5c902c72c8efdb283c
xieh3 <hao.xie@nih.gov>
2021-04-22 16:29:48 -0400

    format

--- next/src/app/modules/search-parameter/search-parameter.component.ts
+++ next/src/app/modules/search-parameter/search-parameter.component.ts
@@ -26,6 +26,7 @@ export class SearchParameterComponent
   extends BaseControlValueAccessor<SearchParameter>
   implements OnInit {
   @Input() fixedResourceType = true;
+  definitions: any;
 
   resourceType: FormControl = new FormControl('');
   resourceTypes: string[] = [];
@@ -45,7 +46,8 @@ export class SearchParameterComponent
 
   constructor(private fhirBackend: FhirBackendService) {
     super();
-    this.resourceTypes = Object.keys(fhirBackend.getCurrentDefinitions().resources);
+    this.definitions = fhirBackend.getCurrentDefinitions();
+    this.resourceTypes = Object.keys(this.definitions.resources);
   }
 
   ngOnInit(): void {
@@ -54,11 +56,13 @@ export class SearchParameterComponent
       map((value) => this._filter(value, this.resourceTypes))
     );
 
-    this.resourceType.valueChanges.subscribe(value => {
-      const match = this.resourceTypes.find(rt => rt === value);
+    this.resourceType.valueChanges.subscribe((value) => {
+      const match = this.resourceTypes.find((rt) => rt === value);
       if (match) {
-        this.selectedResourceType = this.fhirBackend.getCurrentDefinitions().resources[value];
-        this.parameterNames = this.selectedResourceType.searchParameters.map(sp => sp.name);
+        this.selectedResourceType = this.definitions.resources[value];
+        this.parameterNames = this.selectedResourceType.searchParameters.map(
+          (sp) => sp.name
+        );
       }
     });
 
@@ -67,19 +71,22 @@ export class SearchParameterComponent
       map((value) => this._filter(value, this.parameterNames))
     );
 
-    this.parameterName.valueChanges.subscribe(value => {
+    this.parameterName.valueChanges.subscribe((value) => {
       if (this.selectedResourceType) {
-        this.selectedParameter = this.selectedResourceType.searchParameters.find(p => p.name === value);
+        this.selectedParameter = this.selectedResourceType.searchParameters.find(
+          (p) => p.name === value
+        );
         if (this.selectedParameter && this.selectedParameter.valueSet) {
-          this.parameterValues = this.fhirBackend.getCurrentDefinitions().valueSets[this.selectedParameter.valueSet]
-            .map(v => v.display);
+          this.parameterValues = this.definitions.valueSets[
+            this.selectedParameter.valueSet
+          ].map((v) => v.display);
           this.filteredParameterValues = this.parameterValue.valueChanges.pipe(
             startWith(''),
-            map((value) => this._filter(value, this.parameterValues))
+            map((v) => this._filter(v, this.parameterValues))
           );
         }
       }
-    })
+    });
   }
 
   private _filter(
