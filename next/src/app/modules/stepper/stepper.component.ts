e031f0825236c70404a1d9294193f7f04e4e7de1
xieh3 <hao.xie@nih.gov>
2021-06-11 13:27:24 -0400

    load cohort and criteria

--- next/src/app/modules/stepper/stepper.component.ts
+++ next/src/app/modules/stepper/stepper.component.ts
@@ -7,9 +7,11 @@ import {
 } from '../../shared/fhir-backend/fhir-backend.service';
 import { ColumnDescriptionsService } from '../../shared/column-descriptions/column-descriptions.service';
 import { filter, take } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 import { saveAs } from 'file-saver';
 import { ViewCohortPageComponent } from '../step-3-view-cohort-page/view-cohort-page.component';
+import { SearchParameter } from '../../types/search.parameter';
+import Resource = fhir.Resource;
 
 /**
  * The main component provides a wizard-like workflow by dividing content into logical steps.
@@ -63,5 +65,58 @@ export class StepperComponent implements OnDestroy {
       endings: 'native'
     });
     saveAs(blob, `cohort-${objectToSave.data.length}.json`);
+  }
+
+  /**
+   * Process file and load criteria and patient list data.
+   */
+  loadCohort(event): void {
+    if (event.target.files.length === 1) {
+      const reader = new FileReader();
+      const filename = event.target.files[0].name;
+      reader.onload = (loadEvent) => {
+        try {
+          const blobData = JSON.parse(loadEvent.target.result as string);
+          const {
+            serviceBaseUrl,
+            maxPatientCount,
+            rawCriteria,
+            data
+          } = blobData;
+          if (serviceBaseUrl !== this.fhirBackend.serviceBaseUrl) {
+            throw new Error(
+              'Inapplicable data, because it was downloaded from another server.'
+            );
+          }
+          // Set max field value.
+          this.defineCohortComponent.defineCohortForm
+            .get('maxPatientsNumber')
+            .setValue(maxPatientCount);
+          // Set search parameter form values.
+          (rawCriteria as SearchParameter[]).forEach((searchParam) => {
+            this.defineCohortComponent.patientParams.parameterList.push(
+              new FormControl(searchParam)
+            );
+          });
+          // Set patient table data.
+          this.loadPatientsData(data);
+        } catch (e) {
+          alert('Error: ' + e.message);
+        }
+      };
+      reader.readAsText(event.target.files[0]);
+    }
+    event.target.value = '';
+  }
+
+  private loadPatientsData(data: Resource[]): void {
+    this.defineCohortComponent.patientStream = new Subject<Resource>();
+    this.myStepper.next();
+    setTimeout(() => {
+      data.forEach((resource) => {
+        this.defineCohortComponent.patientStream.next(resource);
+      });
+      this.defineCohortComponent.patientStream.complete();
+    });
   }
 }
