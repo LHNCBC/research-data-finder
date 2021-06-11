c6870ccad8f7c8c75aece065fb829d8d14124bdb
sedinkinya <yury.sedinkin@nih.gov>
2021-06-11 17:50:25 -0400

    Rename project, add version and change output dir
    LF-1840

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'next';
-}
+export class AppComponent {}
