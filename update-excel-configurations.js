be371ecb4163ffa201da993afbb6eba375d67e71
xieh3 <hao.xie@nih.gov>
2021-09-22 11:55:32 -0400

    restore file

--- update-excel-configurations.js
+++ update-excel-configurations.js
@@ -4,31 +4,28 @@ const fs = require('fs');
 const filePath = 'src/conf/xlsx/column-and-parameter-descriptions.xlsx';
 const filePath_old = 'src/conf/xlsx/column-and-parameter-descriptions_old.xlsx';
 const file = reader.readFile(filePath);
-fs.rename(filePath, filePath_old, () => {
-  const sheets = file.SheetNames;
-  for (let i = 0; i < sheets.length; i++) {
-    if (i !== 0) {
-      break;
+//fs.rename(filePath, filePath_old, () => {
+const sheets = file.SheetNames;
+const wb = reader.utils.book_new();
+for (let i = 0; i < sheets.length; i++) {
+  let data = [];
+  const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]], {
+    header: 'A',
+    blankrows: true
+  });
+  temp.forEach((res) => {
+    if (res.C === 'search parameter') {
+      res.E = 'hide';
     }
-    let data = [];
-    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]], {
-      header: 'A',
-      blankrows: true
-    });
-    temp.forEach((res) => {
-      if (res.__EMPTY_1 === 'search parameter') {
-        res.__EMPTY_3 = 'hide';
-      }
-      data.push(res);
-    });
-    console.log(data);
+    data.push(res);
+  });
+  console.log(data);
 
-    const ws = reader.utils.json_to_sheet(data, {
-      header: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-      skipHeader: true
-    });
-    const wb = reader.utils.book_new();
-    reader.utils.book_append_sheet(wb, ws, 'Sheet3');
-    reader.writeFile(wb, filePath);
-  }
-});
+  const ws = reader.utils.json_to_sheet(data, {
+    header: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+    skipHeader: true
+  });
+  reader.utils.book_append_sheet(wb, ws, file.SheetNames[i]);
+}
+reader.writeFile(wb, filePath_old);
+//});
