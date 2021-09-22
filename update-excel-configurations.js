3123a699fcab42f0df01117d0b297a9071d9806b
xieh3 <hao.xie@nih.gov>
2021-09-22 11:36:00 -0400

    create new xlsx

--- update-excel-configurations.js
+++ update-excel-configurations.js
@@ -1,32 +1,34 @@
 const reader = require('xlsx');
-const file = reader.readFile(
-  'src/conf/xlsx/column-and-parameter-descriptions.xlsx'
-);
-const sheets = file.SheetNames;
-for (let i = 0; i < sheets.length; i++) {
-  if (i !== 0) {
-    break;
-  }
-  let data = [];
-  const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]], {
-    header: 'A',
-    blankrows: true
-  });
-  temp.forEach((res) => {
-    if (res.__EMPTY_1 === 'search parameter') {
-      res.__EMPTY_3 = 'hide';
-    }
-    data.push(res);
-  });
-  console.log(data);
+const fs = require('fs');
 
-  const ws = reader.utils.json_to_sheet(data, {
-    header: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-    skipHeader: true
-  });
-  reader.utils.book_append_sheet(file, ws, 'Sheet3');
-  reader.writeFile(
-    file,
-    'src/conf/xlsx/column-and-parameter-descriptions.xlsx'
-  );
-}
+const filePath = 'src/conf/xlsx/column-and-parameter-descriptions.xlsx';
+const filePath_old = 'src/conf/xlsx/column-and-parameter-descriptions_old.xlsx';
+const file = reader.readFile(filePath);
+fs.rename(filePath, filePath_old, () => {
+  const sheets = file.SheetNames;
+  for (let i = 0; i < sheets.length; i++) {
+    if (i !== 0) {
+      break;
+    }
+    let data = [];
+    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]], {
+      header: 'A',
+      blankrows: true
+    });
+    temp.forEach((res) => {
+      if (res.__EMPTY_1 === 'search parameter') {
+        res.__EMPTY_3 = 'hide';
+      }
+      data.push(res);
+    });
+    console.log(data);
+
+    const ws = reader.utils.json_to_sheet(data, {
+      header: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+      skipHeader: true
+    });
+    const wb = reader.utils.book_new();
+    reader.utils.book_append_sheet(wb, ws, 'Sheet3');
+    reader.writeFile(wb, filePath);
+  }
+});
