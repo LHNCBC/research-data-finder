ea6e2d458ed409388315d2aca801c3b0a2292d46
xieh3 <hao.xie@nih.gov>
2021-10-21 10:51:56 -0400

    use stdio inherit

--- start-public.js
+++ start-public.js
@@ -1,18 +1,5 @@
 const hostName = require('os').hostname();
 const { spawn } = require('child_process');
-const ls = spawn('ng', ['serve', '--port', '8100', '--host', hostName]);
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-ls.on('error', (error) => {
-  console.log(`error: ${error.message}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+spawn('ng', ['serve', '--port', '8100', '--host', hostName], {
+  stdio: 'inherit'
 });
