2e39768d01c2203d6add59ceaba4d5982dbf9c73
xieh3 <hao.xie@nih.gov>
2022-03-31 11:12:48 -0400

    hide shared header/footer until page loads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import pkg from '../../package.json';
 
 @Component({
@@ -6,6 +6,12 @@ import pkg from '../../package.json';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   version = pkg.version;
+
+  ngAfterViewInit(): void {
+    // Display shared header/footer after Angular page loads.
+    document.getElementById('sharedHeader').style.display = 'block';
+    document.getElementById('sharedFooter').style.display = 'block';
+  }
 }
