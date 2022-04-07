a6117a5ca8c03f4beca07d456a20155b04cd26c4
sedinkinya <yury.sedinkin@nih.gov>
2022-04-07 17:11:05 -0400

    Added e2e tests
    LF-2197

--- test/cypress/integration/baseR4.e2e-spec.ts
+++ test/cypress/integration/baseR4.e2e-spec.ts
@@ -91,12 +91,36 @@ describe('Research Data Finder (baseR4)', () => {
       });
   });
 
+  it('should be an autocomplete field for DocumentReference.contenttype', () => {
+    cy.contains('Add criteria for a record type').click();
+    cy.get('app-autocomplete.resource-type input').type('DocumentReference');
+    cy.contains('Add a criterion for the DocumentReference record').click();
+    cy.get('app-autocomplete.parameter-name input').type('contenttype');
+    cy.get('#autocomplete-test-value-1').focus();
+    cy.get('#completionOptions').should('be.visible');
+    cy.get('#completionOptions > ul > li').should('not.be.empty');
+    cy.get('button.remove-btn').eq(0).click();
+  });
+
+  it('should be an autocomplete field for Encounter.type', () => {
+    cy.contains('Add criteria for a record type').click();
+    cy.get('app-autocomplete.resource-type input').type('Encounter');
+    cy.contains('Add a criterion for the Encounter record').click();
+    cy.get('app-autocomplete.parameter-name input').type('type');
+    cy.get('#autocomplete-test-value-2').focus();
+    cy.get('#completionOptions').should('be.visible');
+    cy.get('#completionOptions > ul > li').should('not.be.empty');
+    cy.get('button.remove-btn').eq(0).click();
+  });
+
   it('should add search criterion', () => {
     cy.contains('Add criteria for a record type').click();
     cy.get('app-autocomplete.resource-type input').type('Observation');
     cy.contains('Add a criterion for the Observation record').click();
     cy.get('app-autocomplete.parameter-name input').type('variable name');
-    cy.get('#code-selector-2').type('Height cm').wait(1000).blur();
+    cy.get('#code-selector-2').type('Height cm');
+    cy.get('#completionOptions').should('be.visible');
+    cy.get('#completionOptions > ul > li:first-child').click();
   });
 
   it('should not allow skipping the View cohort (search for patients) step', (done) => {
