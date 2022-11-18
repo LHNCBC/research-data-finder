af19bc8987f28f58fd21d03b3b9298bc50ab3616
sedinkinya <yury.sedinkin@nih.gov>
2022-11-17 19:24:53 -0500

    Fixed e2e tests after migrating from Angular 12 to 14
    LF-2449

---
+++ test/cypress/cypress.webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const tsConfigFile = path.resolve(__dirname, './tsconfig.json');
+
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.js', '.mjs', '.json']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: tsConfigFile
+          }
+        }
+      },
+      {
+        // Angular linker needed to link partial-ivy code
+        // See https://angular.io/guide/creating-libraries#consuming-partial-ivy-code-outside-the-angular-cli
+        test: /[/\\]@angular[/\\].+\.m?js$/,
+        resolve: {
+          fullySpecified: false
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['@angular/compiler-cli/linker/babel'],
+            compact: false,
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  }
+};
