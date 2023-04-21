7fb53e2ad6ddc7ed6144516307ef0e5fb6409e16
xieh3 <hao.xie@nih.gov>
2023-04-21 11:02:42 -0400

    variable table prefix search

--- src/app/shared/select-records/select-records.service.ts
+++ src/app/shared/select-records/select-records.service.ts
@@ -213,7 +213,10 @@ export class SelectRecordsService {
           // searching the long and short LOINC names.
           query.push(`(${dataFields[key]}:(${value}) OR synonyms:(${value}))`);
         } else {
-          query.push(`${dataFields[key]}:(${value})`);
+          // Use wildcard search for below columns so user can filter by typing a prefix.
+          ['id', 'study_id', 'dataset_id', 'type', 'unit'].includes(key)
+            ? query.push(`${dataFields[key]}:(${value}*)`)
+            : query.push(`${dataFields[key]}:(${value})`);
         }
       }
     });
