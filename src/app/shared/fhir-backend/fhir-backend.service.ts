c016accef89c2fa76094c8a202efbb86dc67f713
xieh3 <hao.xie@nih.gov>
2023-05-03 15:54:38 -0400

    RAS relogin

--- src/app/shared/fhir-backend/fhir-backend.service.ts
+++ src/app/shared/fhir-backend/fhir-backend.service.ts
@@ -27,6 +27,9 @@ import { FhirService } from '../fhir-service/fhir.service';
 import { Router } from '@angular/router';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { RasTokenService } from '../ras-token/ras-token.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
+import { CreateCohortMode } from '../cohort/cohort.service';
 
 // RegExp to modify the URL of requests to the FHIR server.
 // If the URL starts with the substring "$fhir", it will be replaced
@@ -195,13 +198,15 @@ export class FhirBackendService implements HttpBackend {
    *   service instances and injecting them into classes
    * @param liveAnnouncer a service is used to announce messages for screen-reader
    *   users using an aria-live region.
+   * @param dialog MatDialog service from Angular
    */
   constructor(
     private defaultBackend: HttpXhrBackend,
     private fhirService: FhirService,
     private router: Router,
     private injector: Injector,
-    private liveAnnouncer: LiveAnnouncer
+    private liveAnnouncer: LiveAnnouncer,
+    private dialog: MatDialog
   ) {
     this._isSmartOnFhir = getUrlParam('isSmart') === 'true';
     const defaultServer = 'https://lforms-fhir.nlm.nih.gov/baseR4';
@@ -504,20 +509,35 @@ export class FhirBackendService implements HttpBackend {
                 );
                 observer.complete();
               },
-              // In case of failure due to expired TST token sent to dbGaP, we should
-              // probably log out and user can log back in.
-              // According to Eric, dbGaP query will return a 400. Hopefully it should
-              // come with some error message indicating the expired TST token so we can
-              // distinguish this case with others, but we'll have to wait until it's
-              // implemented in dbGaP.
-              ({ status, error }) =>
+              ({ status, error }) => {
                 observer.error(
                   new HttpErrorResponse({
                     status,
                     error,
                     url: fullUrl
                   })
-                )
+                );
+                if (status >= 400 && status < 500) {
+                  const dialogRef = this.dialog.open(AlertDialogComponent, {
+                    data: {
+                      header: 'Alert',
+                      content:
+                        'Looks like the TST token has expired. You will be redirected to re-login.',
+                      hasCancelButton: true
+                    }
+                  });
+                  dialogRef.afterClosed().subscribe((isOk) => {
+                    if (isOk) {
+                      this.injector
+                        .get(RasTokenService)
+                        .login(
+                          this.serviceBaseUrl,
+                          CreateCohortMode.UNSELECTED
+                        );
+                    }
+                  });
+                }
+              }
             );
           });
           // This is the return from the Observable function, which is the
