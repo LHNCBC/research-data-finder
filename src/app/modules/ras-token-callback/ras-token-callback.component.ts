5b4e1fde66a0728ede2fb1ac6e45739706a7d60e
xieh3 <hao.xie@nih.gov>
2023-09-13 09:59:43 -0400

    handle ras error in callback

--- src/app/modules/ras-token-callback/ras-token-callback.component.ts
+++ src/app/modules/ras-token-callback/ras-token-callback.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { RasTokenService } from '../../shared/ras-token/ras-token.service';
-import { FhirBackendService } from '../../shared/fhir-backend/fhir-backend.service';
-import { HttpClient } from '@angular/common/http';
-import { getUrlParam } from '../../shared/utils';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RasTokenService} from '../../shared/ras-token/ras-token.service';
+import {FhirBackendService} from '../../shared/fhir-backend/fhir-backend.service';
+import {HttpClient} from '@angular/common/http';
+import {getUrlParam} from '../../shared/utils';
 
 @Component({
   selector: 'app-ras-token-callback',
@@ -18,35 +18,41 @@ export class RasTokenCallbackComponent implements OnInit {
     private rasToken: RasTokenService,
     private fhirBackend: FhirBackendService,
     private http: HttpClient
-  ) {}
+  ) {
+  }
+
   ngOnInit(): void {
     const encodedTstToken = getUrlParam('tst-token');
-    this.http
-      .get(
-        `${window.location.origin}/rdf-server/tst-return/?tst-token=${encodedTstToken}`,
-        { withCredentials: true }
-      )
-      .subscribe(
-        (data) => {
-          console.log(data);
-          this.error = null;
-          this.rasToken.rasTokenValidated = true;
-          this.rasToken.isRasCallbackNavigation = true;
-          sessionStorage.setItem('dbgapTstToken', data['message']['tst']);
-          const server = sessionStorage.getItem('dbgapRasLoginServer');
-          this.fhirBackend.serviceBaseUrl = server;
-          this.router.navigate(['/'], {
-            queryParams: {
-              'alpha-version': 'enable',
-              server
-            },
-            replaceUrl: true
-          });
-        },
-        (err) => {
-          this.rasToken.rasTokenValidated = false;
-          this.error = err;
-        }
-      );
+    if (encodedTstToken) {
+      this.http
+        .get(
+          `${window.location.origin}/rdf-server/tst-return/?tst-token=${encodedTstToken}`,
+          {withCredentials: true}
+        )
+        .subscribe(
+          (data) => {
+            console.log(data);
+            this.error = null;
+            this.rasToken.rasTokenValidated = true;
+            this.rasToken.isRasCallbackNavigation = true;
+            sessionStorage.setItem('dbgapTstToken', data['message']['tst']);
+            const server = sessionStorage.getItem('dbgapRasLoginServer');
+            this.fhirBackend.serviceBaseUrl = server;
+            this.router.navigate(['/'], {
+              queryParams: {
+                'alpha-version': 'enable',
+                server
+              },
+              replaceUrl: true
+            });
+          },
+          (err) => {
+            this.rasToken.rasTokenValidated = false;
+            this.error = err.error || 'TST decode error';
+          }
+        );
+    } else {
+      this.error = getUrlParam('error') || 'RAS error';
+    }
   }
 }
