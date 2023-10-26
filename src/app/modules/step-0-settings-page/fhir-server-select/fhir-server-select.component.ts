530e455e7b2cd9816422c5e2ffb321178feacc78
xieh3 <hao.xie@nih.gov>
2023-10-26 16:30:58 -0400

    remove immport server from list

--- src/app/modules/step-0-settings-page/fhir-server-select/fhir-server-select.component.ts
+++ src/app/modules/step-0-settings-page/fhir-server-select/fhir-server-select.component.ts
@@ -53,10 +53,6 @@ export class FhirServerSelectComponent
         'https://launch.smarthealthit.org/v/r4/sim/eyJoIjoiMSIsImoiOiIxIn0/fhir'
     },
     {
-      description: 'immport (HAPI FHIR R5) server',
-      url: 'https://fhir.dev.immport.org/fhir'
-    },
-    {
       description: 'Google Health API',
       url: 'https://healthcare.googleapis.com/v1/projects/lhncbc-fhir-tools/locations/us-east4/datasets/lforms-fhir-data/fhirStores/R4-2/fhir'
     }
