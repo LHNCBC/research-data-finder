1c6339137c076d3e5b00453e746d1081b89e7c03
xieh3 <hao.xie@nih.gov>
2023-11-30 15:32:01 -0500

    disable step header based on create cohort mode selection

--- src/app/modules/stepper/stepper.component.ts
+++ src/app/modules/stepper/stepper.component.ts
@@ -68,10 +68,7 @@ export class StepperComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('selectAnActionStep') set _selectAnActionStep(step: MatStep) {
     this.selectAnActionStep = step;
     if (step) {
-      // If RAS login is required, mark the step incomplete to disable the header of the next step.
-      step.completed = !this.enableRas ||
-        !this.fhirBackend.isDbgap(this.fhirBackend.serviceBaseUrl) ||
-        this.rasToken.rasTokenValidated;
+      step.completed = false;
     }
   }
   selectAnActionStep: MatStep;
@@ -94,7 +91,15 @@ export class StepperComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild(SelectAnAreaOfInterestComponent)
   public selectAreaOfInterestComponent: SelectAnAreaOfInterestComponent;
-  @ViewChild(SelectAnActionComponent)
+  @ViewChild(SelectAnActionComponent) set _selectAnActionComponent(comp: SelectAnActionComponent) {
+    this.selectAnActionComponent = comp;
+    if (comp) {
+      comp.createCohortMode.valueChanges.subscribe((value) => {
+        // If RAS login is required, mark the step incomplete to disable the header of the next step.
+        this.selectAnActionStep.completed = !this.isRasLoginRequired(value);
+      });
+    }
+  }
   public selectAnActionComponent: SelectAnActionComponent;
   @ViewChild(DefineCohortPageComponent)
   public defineCohortComponent: DefineCohortPageComponent;
@@ -612,23 +617,27 @@ export class StepperComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   /**
-   * Contact rdf-server for dbGap login, if required.
+   * Returns whether RAS login is required.
    */
-  onSelectAnActionNext(createCohortModeValue: CreateCohortMode): void {
-    if (
-      this.enableRas &&
+  isRasLoginRequired(createCohortModeValue: CreateCohortMode): boolean {
+    return this.enableRas &&
       this.fhirBackend.isDbgap(this.fhirBackend.serviceBaseUrl) &&
       [CreateCohortMode.BROWSE, CreateCohortMode.SEARCH].includes(
         createCohortModeValue
       ) &&
-      !this.rasToken.rasTokenValidated
-    ) {
+      !this.rasToken.rasTokenValidated;
+  }
+
+  /**
+   * Contact rdf-server for dbGap login, if required.
+   */
+  onSelectAnActionNext(createCohortModeValue: CreateCohortMode): void {
+    if (this.isRasLoginRequired(createCohortModeValue)) {
       this.rasToken.login(
         this.fhirBackend.serviceBaseUrl,
         createCohortModeValue
       );
     } else {
-      this.selectAnActionStep.completed = true;
       this.stepper.next();
     }
   }
